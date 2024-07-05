38566dc80605047793f9d57044e6f95ff15ce765
Vladyslav Antonchyk <109813312+BlaSSSTTT@users.noreply.github.com>
2024-07-05 22:29:58 +0300

    Init commit

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -2,4 +2,15 @@ const mongoose = require("mongoose");
 
 const connectionURL = "mongodb+srv://antonchykvladyslav:gbY8WOPNtNJmGymT@cluster0.3nt7jvy.mongodb.net/task-manager-api?retryWrites=true&w=majority&appName=Cluster0";
 
-mongoose.connect(connectionURL)
+mongoose.connect(connectionURL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  ssl: true,
+  tlsInsecure: true,
+})
+.then(() => {
+  console.log('Successfully connected to MongoDB');
+})
+.catch((err) => {
+  console.error('Error connecting to MongoDB:', err);
+});
